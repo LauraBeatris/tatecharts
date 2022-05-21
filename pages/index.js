374bf62ea2fe0cb52d0973f321368de6a100ffaf
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-05-21 15:42:08 +0200

    Add ESLint initial config

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
-export default function Home() {
+export default function Home () {
   return (
     <div>
       <Head>
         <title>Tate Charts</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <div className='album-logo'>
-        <Image src='/images/iutticf-logo.png' layout='fill'></Image>
+        <Image
+          src='/images/iutticf-logo.png'
+          alt='I Used To Think I Could Fly Album Logo'
+          layout='fill'
+        />
       </div>
     </div>
   )
