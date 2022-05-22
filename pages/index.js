2b702fc1309b8b86de7b26fdf7fb0964bee01746
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-05-22 10:58:33 +0200

    Install Chakra-UI dependencies and clean global CSS code

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
-
 export default function Home () {
   return (
-    <div>
-      <Head>
-        <title>Tate Charts</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-
-      <div className='album-logo'>
-        <Image
-          src='/images/iutticf-logo.png'
-          alt='I Used To Think I Could Fly Album Logo'
-          layout='fill'
-        />
-      </div>
-    </div>
+    <main />
   )
 }
