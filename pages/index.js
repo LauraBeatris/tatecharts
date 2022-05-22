842353d1d582f66a3956fd2587aca42326173b9c
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-05-22 11:16:09 +0200

    Implement screen layout

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,52 @@
+import { Container, Flex, VStack } from '@chakra-ui/react'
+import Image from 'next/image'
+
+const imageStyle = {
+  filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))'
+}
+
+function AlbumLogo () {
+  return (
+    <Flex
+      width={['100%', '457px']}
+      height='170px'
+      position='relative'
+    >
+      <Image
+        src='/images/iutticf-logo.png'
+        alt='Imagem da balança do direito'
+        layout='fill'
+        style={imageStyle}
+      />
+    </Flex>
+  )
+}
+
+function ScreenLayout ({ children }) {
+  return (
+    <VStack
+      as='main'
+      width='100vw'
+      height='100vh'
+      backgroundColor='pink.300'
+    >
+      <Container
+        maxW='2xl'
+        height='100%'
+        paddingX='5'
+        paddingY='10'
+        centerContent
+      >
+        {children}
+      </Container>
+    </VStack>
+  )
+}
+
 export default function Home () {
   return (
-    <main />
+    <ScreenLayout>
+      <AlbumLogo />
+    </ScreenLayout>
   )
 }
