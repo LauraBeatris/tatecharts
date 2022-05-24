8c0da69a94ea9e0f8382c689058e475272fa5720
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-05-24 12:28:14 +0200

    Display spotify streams count

--- components/Tables.js
+++ components/Tables.js
@@ -1,135 +1,159 @@
 
 import Image from 'next/image'
-import { Flex, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr, VStack } from '@chakra-ui/react'
-
-const mapPlatformSingleLinks = {
-  spotify: 'https://open.spotify.com/track/3I1Smy5zhzNEc9grpjwY1s?si=2af28cac73fa4e1b',
-  deezer: 'https://deezer.page.link/wekUBAJQDUXmWSAWA',
-  appleMusic: 'https://music.apple.com/us/album/what-would-you-do/1620209610?i=1620209736'
-}
+import { Flex, Skeleton, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr, VStack } from '@chakra-ui/react'
+import { getTrack } from 'pages/api/track'
+import { useQuery } from 'react-query'
+import { mapSongsISRC } from 'isrc'
 
 export function LastSingleStatsTable ({ tableContainerProps }) {
-  const navigateToSingleOnPlatform = (platform) => () => {
-    window.open(mapPlatformSingleLinks[platform])
+  const { data, isLoading } = useQuery('track', () =>
+    getTrack({ isrc: mapSongsISRC['what-would-you-do'] })
+  )
+
+  const { stats, links } = data ?? {}
+
+  const navigateToSingleOnPlatform = (platformName) => () => {
+    const { spotify, appleMusic, deezer } = links ?? {}
+    const url = {
+      deezer: deezer.url,
+      spotify: spotify.url,
+      appleMusic: appleMusic.url
+    }[platformName]
+
+    window.open(url)
   }
 
   return (
     <VStack width='100%'>
-      <TableContainer
+      <Skeleton
         width='100%'
-        borderRadius='12px'
-        backgroundColor='tableBg'
-        {...tableContainerProps}
+        startColor='tableBg'
+        isLoaded={!isLoading}
+        borderRadius='12'
       >
-        <Table variant='simple' size='sm'>
-          <Thead>
-            <Tr>
-              <Th color='black'>
-                Platform
-              </Th>
-              <Th color='black' isNumeric>Streams</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            <Tr
-              cursor='pointer'
-              onClick={navigateToSingleOnPlatform('spotify')}
-            >
-              <Td color='black'>
-                <Flex alignItems='center'>
-                  <Flex width='25px' height='25px' position='relative'>
-                    <Image
-                      src='/images/icons/spotify.png'
-                      layout='fill'
-                      alt='Spotify Icon'
-                    />
+        <TableContainer
+          width='100%'
+          borderRadius='12px'
+          backgroundColor='tableBg'
+          {...tableContainerProps}
+        >
+          <Table variant='simple' size='sm'>
+            <Thead>
+              <Tr>
+                <Th color='black'>
+                  Platform
+                </Th>
+                <Th color='black' isNumeric>Streams</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr
+                cursor='pointer'
+                onClick={navigateToSingleOnPlatform('spotify')}
+              >
+                <Td color='black'>
+                  <Flex alignItems='center'>
+                    <Flex width='25px' height='25px' position='relative'>
+                      <Image
+                        src='/images/icons/spotify.png'
+                        layout='fill'
+                        alt='Spotify Icon'
+                      />
+                    </Flex>
+
+                    <Text marginLeft='2'>Spotify</Text>
                   </Flex>
+                </Td>
+                <Td color='black' isNumeric>{stats?.spotify?.streams_total}</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Skeleton>
 
-                  <Text marginLeft='2'>Spotify</Text>
-                </Flex>
-              </Td>
-              <Td color='black' isNumeric>200</Td>
-            </Tr>
-          </Tbody>
-        </Table>
-      </TableContainer>
-
-      <TableContainer
+      <Skeleton
         width='100%'
-        borderRadius='12px'
-        backgroundColor='tableBg'
-        {...tableContainerProps}
+        startColor='tableBg'
+        isLoaded={!isLoading}
+        borderRadius='12'
       >
-        <Table variant='simple' size='sm'>
-          <Thead>
-            <Tr>
-              <Th color='black'>
-                Platform
-              </Th>
-              <Th color='black' isNumeric>Chart Position</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            <Tr
-              cursor='pointer'
-              onClick={navigateToSingleOnPlatform('spotify')}
-            >
-              <Td color='black'>
-                <Flex alignItems='center'>
-                  <Flex width='25px' height='25px' position='relative'>
-                    <Image
-                      src='/images/icons/spotify.png'
-                      layout='fill'
-                      alt='Spotify Icon'
-                    />
-                  </Flex>
+        <TableContainer
+          width='100%'
+          borderRadius='12px'
+          backgroundColor='tableBg'
+          {...tableContainerProps}
+        >
+          <Table variant='simple' size='sm'>
+            <Thead>
+              <Tr>
+                <Th color='black'>
+                  Platform
+                </Th>
+                <Th color='black' isNumeric>Chart Position</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr
+                cursor='pointer'
+                onClick={navigateToSingleOnPlatform('spotify')}
+              >
+                <Td color='black'>
+                  <Flex alignItems='center'>
+                    <Flex width='25px' height='25px' position='relative'>
+                      <Image
+                        src='/images/icons/spotify.png'
+                        layout='fill'
+                        alt='Spotify Icon'
+                      />
+                    </Flex>
 
-                  <Text marginLeft='2'>Spotify</Text>
-                </Flex>
-              </Td>
-              <Td color='black' isNumeric>#1</Td>
-            </Tr>
-            <Tr
-              cursor='pointer'
-              onClick={navigateToSingleOnPlatform('deezer')}
-            >
-              <Td color='black'>
-                <Flex alignItems='center'>
-                  <Flex width='25px' height='25px' position='relative'>
-                    <Image
-                      src='/images/icons/deezer.png'
-                      alt='Deezer Icon'
-                      layout='fill'
-                    />
+                    <Text marginLeft='2'>Spotify</Text>
                   </Flex>
+                </Td>
+                <Td color='black' isNumeric>#1</Td>
+              </Tr>
+              <Tr
+                cursor='pointer'
+                onClick={navigateToSingleOnPlatform('deezer')}
+              >
+                <Td color='black'>
+                  <Flex alignItems='center'>
+                    <Flex width='25px' height='25px' position='relative'>
+                      <Image
+                        src='/images/icons/deezer.png'
+                        alt='Deezer Icon'
+                        layout='fill'
+                      />
+                    </Flex>
 
-                  <Text marginLeft='2'>Deezer</Text>
-                </Flex>
-              </Td>
-              <Td color='black' isNumeric>#2</Td>
-            </Tr>
-            <Tr
-              cursor='pointer'
-              onClick={navigateToSingleOnPlatform('appleMusic')}
-            >
-              <Td color='black'>
-                <Flex alignItems='center'>
-                  <Flex width='25px' height='25px' position='relative'>
-                    <Image
-                      src='/images/icons/apple-music.png'
-                      alt='Apple Music'
-                      layout='fill'
-                    />
+                    <Text marginLeft='2'>Deezer</Text>
                   </Flex>
+                </Td>
+                <Td color='black' isNumeric>#2</Td>
+              </Tr>
+              <Tr
+                cursor='pointer'
+                onClick={navigateToSingleOnPlatform('appleMusic')}
+              >
+                <Td color='black'>
+                  <Flex alignItems='center'>
+                    <Flex width='25px' height='25px' position='relative'>
+                      <Image
+                        src='/images/icons/apple-music.png'
+                        alt='Apple Music'
+                        layout='fill'
+                      />
+                    </Flex>
 
-                  <Text marginLeft='2'>Apple Music</Text>
-                </Flex>
-              </Td>
-              <Td color='black' isNumeric>#3</Td>
-            </Tr>
-          </Tbody>
-        </Table>
-      </TableContainer>
+                    <Text marginLeft='2'>Apple Music</Text>
+                  </Flex>
+                </Td>
+                <Td color='black' isNumeric>#3</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Skeleton>
     </VStack>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,22 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { Global } from '@emotion/react'
 import { theme } from 'styles/theme'
 import { global } from 'styles/global'
+import {
+  QueryClient,
+  QueryClientProvider
+} from 'react-query'
+import { useState } from 'react'
 
 function MyApp ({ Component, pageProps }) {
+  const [queryClient] = useState(() => new QueryClient())
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Global styles={global} />
       <ChakraProvider resetCSS theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
-    </>
+    </QueryClientProvider>
   )
 }
 
