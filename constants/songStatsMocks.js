206d268b04a4aeea99e850f75e0d273915cceeb8
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-07-08 19:36:37 +0200

    Post tweet with monthly listeners

---
+++ constants/songStatsMocks.js
@@ -0,0 +1,127 @@
+export const mockSpotifyAudience = {
+  source: 'spotify',
+  data: {
+    monthly_listeners: [
+      {
+        country_code: 'US',
+        city_name: 'Atlanta',
+        current_listeners: 180993,
+        peak_listeners: 215551
+      },
+      {
+        country_code: 'US',
+        city_name: 'Austin',
+        current_listeners: 76694,
+        peak_listeners: 96418
+      },
+      {
+        country_code: 'US',
+        city_name: 'Boston',
+        current_listeners: 74104,
+        peak_listeners: 81993
+      },
+      {
+        country_code: 'US',
+        city_name: 'Brooklyn',
+        current_listeners: 178252,
+        peak_listeners: 187625
+      },
+      {
+        country_code: 'US',
+        city_name: 'Charlotte',
+        current_listeners: 97916,
+        peak_listeners: 121774
+      },
+      {
+        country_code: 'US',
+        city_name: 'Chicago',
+        current_listeners: 260978,
+        peak_listeners: 307425
+      },
+      {
+        country_code: 'US',
+        city_name: 'Cleveland',
+        current_listeners: 77891,
+        peak_listeners: 78655
+      },
+      {
+        country_code: 'US',
+        city_name: 'Dallas',
+        current_listeners: 216457,
+        peak_listeners: 243073
+      },
+      {
+        country_code: 'US',
+        city_name: 'Denver',
+        current_listeners: 105269,
+        peak_listeners: 139911
+      },
+      {
+        country_code: 'US',
+        city_name: 'Houston',
+        current_listeners: 158136,
+        peak_listeners: 170288
+      },
+      {
+        country_code: 'US',
+        city_name: 'Los Angeles',
+        current_listeners: 252138,
+        peak_listeners: 303256
+      },
+      {
+        country_code: 'US',
+        city_name: 'Miami',
+        current_listeners: 98423,
+        peak_listeners: 119293
+      },
+      {
+        country_code: 'DE',
+        city_name: 'Berlin',
+        current_listeners: 144298,
+        peak_listeners: 150269
+      },
+      {
+        country_code: 'DE',
+        city_name: 'Frankfurt am Main',
+        current_listeners: 111124,
+        peak_listeners: 120987
+      },
+      {
+        country_code: 'DE',
+        city_name: 'Hamburg',
+        current_listeners: 138436,
+        peak_listeners: 148237
+      },
+      {
+        country_code: 'DE',
+        city_name: 'Munich',
+        current_listeners: 99717,
+        peak_listeners: 145372
+      },
+      {
+        country_code: 'DE',
+        city_name: 'Stuttgart',
+        current_listeners: 65951,
+        peak_listeners: 112128
+      },
+      {
+        country_code: 'GB',
+        city_name: 'Birmingham',
+        current_listeners: 134114,
+        peak_listeners: 140920
+      },
+      {
+        country_code: 'GB',
+        city_name: 'Bristol',
+        current_listeners: 94818,
+        peak_listeners: 102026
+      },
+      {
+        country_code: 'GB',
+        city_name: 'Manchester',
+        current_listeners: 94164,
+        peak_listeners: 94164
+      }
+    ]
+  }
+}
